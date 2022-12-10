85025084f11910e5b7a87ee8c028991a5a20fb6a
Dylan <103233743+blxnkindex@users.noreply.github.com>
2022-12-10 14:40:34 +1100

    Updates to default cog
    - Added help command which displays all bot commands
    - Moved coin flip command to misc cog

--- default.py
+++ default.py
@@ -7,24 +7,33 @@ from discord import Colour
 
 import random
 
-
-
 class Default(commands.Cog, name = 'default'):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.hybrid_command(
-        name = 'test',
-        description = 'TESTING'
+        name='help',
+        description='List all bot commands'
     )
     @app_commands.guilds(discord.Object(id = int(os.getenv('MAIN_SERVER'))))
-    @commands.has_permissions(administrator = True)
-    async def test(self, ctx: Context):
-        await ctx.send(embed = discord.Embed(
-            title='test command', 
-            description='Description', 
-            colour=rand_colour()
-        ))
+    async def help(self, ctx):
+        embed = discord.Embed(
+            title='Help 💬',
+            description='Available commands:',
+            color=0xFFFFF
+        )
+
+        for i in self.bot.cogs:
+            cmds = []
+            for command in self.bot.get_cog(i.lower()).get_commands():
+                description = command.description.partition('\n')[0]
+                cmds.append(f'>{command.name}: {description}')
+                cmd = "\n".join(cmds)
+            embed.add_field(name=i.capitalize() + 'commands',
+                            value=f'```{cmd}```', inline=False)
+
+        await ctx.send(embed=embed)
+
 
     @commands.hybrid_command(
         name = 'info',
@@ -32,34 +41,20 @@ class Default(commands.Cog, name = 'default'):
         aliases = ['serverinfo']
     )
     @app_commands.guilds(discord.Object(id = int(os.getenv('MAIN_SERVER'))))
-    async def info(self, ctx: Context):
+    async def info(self, ctx):
         embed = discord.Embed(
-            title=str(ctx.guild.name),
-            description=str(ctx.guild),
+            title=str(ctx.author),
+            description=str(ctx.guild.name),
             colour=rand_colour()
         )
         if ctx.guild.icon:
             embed.set_thumbnail(url=ctx.guild.icon.url)
 
-        embed.add_field(
-            name='Server ID',
-            value=ctx.guild.id
-        )
-        embed.add_field(
-            name='Members',
-            value=ctx.guild.member_count
-        )
-        embed.add_field(
-            name='Channels',
-            value=f'{len(ctx.guild.channels)}'
-        )
-        embed.add_field(
-            name='Roles',
-            value=len(ctx.guild.roles)
-        )
-        embed.set_footer(
-            text=f'Server Made: {ctx.guild.created_at}'
-        )
+        embed.add_field(name='Server ID', value=ctx.guild.id)
+        embed.add_field(name='Members', value=ctx.guild.member_count)
+        embed.add_field(name='Channels', value=f'{len(ctx.guild.channels)}')
+        embed.add_field(name='Roles', value=len(ctx.guild.roles))
+        embed.set_footer(text=f'Server Made: {ctx.guild.created_at}')
         await ctx.send(embed=embed)
 
     @commands.hybrid_command(
@@ -67,32 +62,12 @@ class Default(commands.Cog, name = 'default'):
         description='Check bot delay',
     )
     @app_commands.guilds(discord.Object(id = int(os.getenv('MAIN_SERVER'))))
-    async def ping(self, ctx: Context):
-        await ctx.send(f'Pong! Latency is `{round(self.bot.latency * 1000, 1)}ms`')
-
-    @commands.hybrid_command(
-        name = 'flip',
-        description = 'Flip a coin',
-        aliases = ['coinflip', 'flipcoin']
-    )
-    @app_commands.guilds(discord.Object(id = int(os.getenv('MAIN_SERVER'))))
-    async def flip(self, ctx: Context):
-        result = random.choice(['Heads!', 'Tails!'])
-        embed = discord.Embed(
-            title=result,
-            colour=rand_colour()
-        )
-        if result == 'Heads!':
-            embed.set_image(url='https://static.ayana.io/commands/flipcoin/heads.png')
-        else:
-            embed.set_image(url='https://static.ayana.io/commands/flipcoin/tails.png')
-        await ctx.send(embed=embed)
+    async def ping(self, ctx):
+        await ctx.send(f'🏓 Pong! Latency is `{round(self.bot.latency * 1000, 1)}ms`')
 
 async def setup(bot):
     await bot.add_cog(Default(bot))
 
-
 # Helpers
-
 def rand_colour():
     return Colour.from_rgb(random.randint(0, 255),random.randint(0, 255),random.randint(0, 255))
