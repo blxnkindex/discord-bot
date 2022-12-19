993b99a1f92e84b7dd57b08bac884eddc4251cc1
Dylan <103233743+blxnkindex@users.noreply.github.com>
2022-12-19 15:26:14 +1100

    Update default.py
    - Changed all command decorators to be one line
    
    - Changed help to not print owner or hidden commands

--- default.py
+++ default.py
@@ -11,35 +11,30 @@ class Default(commands.Cog, name = 'default'):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.hybrid_command(
-        name='help',
-        description='List all bot commands'
-    )
+    @commands.hybrid_command(name='help', description='List all bot commands')
     @app_commands.guilds(discord.Object(id = int(os.getenv('MAIN_SERVER'))))
     async def help(self, ctx):
+        hidden = [':)', ':(']
         embed = discord.Embed(
             title='Help 💬',
-            description='Available commands:',
+            description='**Available commands:**',
             color=0xFFFFF
         )
-
         for i in self.bot.cogs:
             cmds = []
-            for command in self.bot.get_cog(i.lower()).get_commands():
-                description = command.description.partition('\n')[0]
-                cmds.append(f'>{command.name}: {description}')
-                cmd = "\n".join(cmds)
-            embed.add_field(name=i.capitalize() + ' commands',
-                            value=f'```{cmd}```', inline=False)
+            if not i.lower() == 'owner':
+                for command in self.bot.get_cog(i.lower()).get_commands():
+                    if command.name not in hidden:
+                        description = command.description.partition('\n')[0]
+                        cmds.append(f'>{command.name} -  {description}')
+                        cmd = "\n".join(cmds)
+                embed.add_field(name=i.capitalize() + ' commands',
+                                value=f'```{cmd}```', inline=False)
 
         await ctx.send(embed=embed)
 
 
-    @commands.hybrid_command(
-        name = 'info',
-        description = 'Get some info about the server',
-        aliases = ['serverinfo']
-    )
+    @commands.hybrid_command(name = 'info', description = 'Get some info about the server', aliases = ['serverinfo'])
     @app_commands.guilds(discord.Object(id = int(os.getenv('MAIN_SERVER'))))
     async def info(self, ctx):
         embed = discord.Embed(
@@ -57,10 +52,7 @@ class Default(commands.Cog, name = 'default'):
         embed.set_footer(text=f'Server Made: {ctx.guild.created_at}')
         await ctx.send(embed=embed)
 
-    @commands.hybrid_command(
-        name='ping',
-        description='Check bot delay',
-    )
+    @commands.hybrid_command(name='ping', description='Check bot delay',)
     @app_commands.guilds(discord.Object(id = int(os.getenv('MAIN_SERVER'))))
     async def ping(self, ctx):
         await ctx.send(f'🏓 Pong! Latency is `{round(self.bot.latency * 1000, 1)}ms`')
