22e5d41ba718aa372e68b17b7e5954ab17ea6578
Dylan <103233743+blxnkindex@users.noreply.github.com>
2023-03-29 06:01:41 +1100

    Add val elo history command and beta val mmr estimate tool
    - Added valelohistory command
        - functions basically like the ingame match history (shows win/loss and rr gain/loss)
    
    - Added valmmr command. Uses match history api data (same as valhistory) to estimate player mmr and calculates rr variance

--- cogs/valorant.py
+++ cogs/valorant.py
@@ -1,14 +1,12 @@
 import discord
-import os
-from discord import app_commands
 from discord.ext import commands
 import requests
 import json
 import DiscordUtils
 import random
-from discord.ext.commands import Context
+import datetime
 
-from utils import rank_color, create_scoreboard_embed, get_character, get_map_image, process_team, make_leaderboard_embed
+from utils import rank_color, create_scoreboard_embed, get_map_image, make_leaderboard_embed, parseElo, createMmrEmbed, calcVariance
 
 class Valorant(commands.Cog, name = 'valorant'):
     def __init__(self, bot):
@@ -111,6 +109,70 @@ class Valorant(commands.Cog, name = 'valorant'):
             await paginator.run(embeds)
         else:
             return await ctx.send('Failed to find leaderboard. :(', delete_after=15)
+        
+    @commands.hybrid_command(name = 'valquickhistory', description = 'Gets quick elo history of account')
+    async def valquickhistory(self, ctx, riotid, region='ap'):
+        if region not in ['na', 'eu', 'ap', 'kr']:
+            return await ctx.send('Invalid region (na, eu, ap, kr)', delete_after=15)
+        name = riotid.split('#')[0]
+        tag = riotid.split('#')[1]
+        try:
+            async with ctx.typing():
+                response = requests.get(f'https://api.henrikdev.xyz/valorant/v1/mmr-history/{region}/{name}/{tag}')
+            if response.status_code == 200:
+                info = json.loads(json.dumps(response.json(), indent=4))
+                embeds = []
+                for i in range(0,len(info['data']),5):
+                    embeds.append(self.makeQuickHistoryEmbed(info, i))
+                if len(embeds) == 1:
+                    await ctx.send(embed = embeds.pop(0))
+                elif len(embeds) > 1:
+                    paginator = DiscordUtils.Pagination.CustomEmbedPaginator(ctx)
+                    paginator.add_reaction('⏪', "back")
+                    paginator.add_reaction('⏩', "next")
+                    await paginator.run(embeds)
+            else:
+                return await ctx.send('Failed to find leaderboard. :(', delete_after=15)
+        except Exception as e:
+            return await ctx.send(f'Error occured {e}')
+
+    def makeQuickHistoryEmbed(self, info, start):
+        color = rank_color(info["data"][start]["currenttierpatched"].replace(" ", ""))
+        embed = discord.Embed(title=f'{info["name"]}#{info["tag"]}', color=color)
+        for i in range(start, min(start + 5, len(info['data']))):
+            game = info['data'][i]
+            timePlayed = datetime.datetime.fromtimestamp(game['date_raw'])
+
+            rankString = str(game['currenttierpatched']) + ' ' + str(int(game['ranking_in_tier']) - int(game['mmr_change_to_last_game']))+'rr' + ' --> ' + str(game['ranking_in_tier']) + 'rr' + ' Net elo: ' + str(game['mmr_change_to_last_game'])
+            embed.add_field(name=timePlayed.strftime("%A, %B %d, %H:%M"), value=f'```{rankString}```', inline=False)
+            
+        return embed
+
+    @commands.hybrid_command(name = 'valmmr', description = 'Calculates an estimation of your hidden elo')
+    async def valmmr(self, ctx, riotid, region='ap'):
+        if not '#' in riotid or region not in ['na', 'eu', 'ap', 'kr']:
+            return await ctx.send('Invalid account (name#id, surround with "" if you have spaces in name) or region (na, eu, ap, kr)', delete_after=15)
+        name = riotid.split('#')[0]
+        tag = riotid.split('#')[1]
+        try:
+            async with ctx.typing():
+                response = requests.get(f'https://api.henrikdev.xyz/valorant/v3/matches/{region}/{name}/{tag}?filter=competitive')
+                if response.status_code == 200:
+                    info = json.loads(json.dumps(response.json(), indent=4))
+                    if not info['data']:
+                        return await ctx.send(f'API returned no games...')
+                    elos = []   
+                    for game in info['data']:
+                        for player in game['players']['all_players']:
+                            elo = parseElo(player['currenttier_patched'])
+                            elos.append(elo)
+                    variance = calcVariance(region, name, tag)
+                    response = requests.get(f'https://api.kyroskoh.xyz/valorant/v1/mmr/{region}/{name}/{tag}?show=eloonly&display=0')
+                    rawElo = int(response.text.replace('MMR Elo: ', '').replace('.', ''))
+                    hiddenElo = int(sum(elos)/len(elos))
+                    return await ctx.send(embed=createMmrEmbed(hiddenElo, rawElo, variance, name + '#' + tag))
+        except Exception as e:
+            return await ctx.send(f'Error occured {e}')
 
 async def setup(bot):
     await bot.add_cog(Valorant(bot))
